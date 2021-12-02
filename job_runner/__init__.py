61eee0ce95cdab8c6ba0ec94eb08136d5c54f303
Adam Peacock <adam@thepeacock.net>
2021-12-02 13:13:22 -0500

    Fix the singleton import

--- job_runner/__init__.py
+++ job_runner/__init__.py
@@ -1,8 +1,8 @@
 """Job tracking library"""
 
-from . import singleton
+import job_runner.singlton
 
 # These get exposed into job_runner directly, and are, in 99% of cases, the only
 # public API
-find_jobs = singleton.find_jobs
-schedule = singleton.schedule
+find_jobs = job_runner.singlton.find_jobs
+schedule = job_runner.singlton.schedule
