03e0ec881d665deaececf0889a3662a4c361ba44
Adam Peacock <adam@thepeacock.net>
2022-04-29 11:58:36 -0400

    Update job runner for better filtering and timing warnings

--- job_runner/management/commands/run_jobs.py
+++ job_runner/management/commands/run_jobs.py
@@ -1,5 +1,6 @@
 """Command line interface to the job runner"""
 
+from datetime import timedelta
 import sys
 from threading import Event
 from random import random
@@ -22,9 +23,22 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument(
-            "--job-name",
+            "--include-job",
             nargs="+",
-            help="A job name to run. If not job names are provided, all jobs will be run",
+            dest="include_jobs",
+            default=[],
+            help=(
+                "A job name to include. If no jobs are included, "
+                "all jobs except those excluded will be included"
+            ),
+        )
+
+        parser.add_argument(
+            "--exclude-job",
+            nargs="+",
+            dest="exclude_jobs",
+            default=[],
+            help="A job name to exclude, which takes prescience over included jobs",
         )
 
         parser.add_argument(
@@ -55,11 +69,32 @@ class Command(BaseCommand):
         self,
         stop_after: int = 0,
         stop_after_variance: int = 0,
-        job_name: List[str] = [],
+        include_jobs: List[str] = [],
+        exclude_jobs: List[str] = [],
         *args,
         **options
     ):
         log = logger.bind()
+
+        full_job_list = import_jobs()
+        to_execute = full_job_list[:]
+
+        if include_jobs:
+            to_execute = [job for job in to_execute if job.name in include_jobs]
+
+        to_execute = [job for job in to_execute if job.name not in exclude_jobs]
+
+        log.info(
+            "Job list has been computed",
+            to_run=sorted([job.name for job in to_execute]),
+            to_skip=sorted(
+                [job.name for job in full_job_list if job not in to_execute]
+            ),
+        )
+
+        if not to_execute:
+            log.error("There are no jobs to run, exiting")
+            sys.exit(1)
 
         coordinator = Coordinator()
         coordinator.start()
@@ -73,33 +108,43 @@ class Command(BaseCommand):
         signal.signal(signal.SIGTERM, stop_signal_handler)
 
         try:
-            got_job = False
-            for job in import_jobs():
-                if not job_name or job.name in job_name:
-                    log.info("Adding job", job_name=job.name)
-                    coordinator.add(job)
-                    got_job = True
-                else:
-                    log.debug("Skipping job", job_name=job.name)
+            for job in to_execute:
+                log.info("Adding job", job_name=job.name)
+                coordinator.add(job)
 
         except Exception as exc:
-            coordinator.request_stop()
-            raise exc
-
-        if not got_job:
-            log.error("Coordinator is not running any jobs, exiting")
+            log.exception("Got exception when adding jobs", error=str(exc))
             coordinator.request_stop()
             coordinator.join()
             sys.exit(1)
 
+        # The coordinator has started successfully
         done_evt = Event()
         if stop_after:
-            variance = random() * stop_after_variance
-            final_delay = stop_after + variance
+            min_runtime = timedelta(seconds=stop_after)
+            max_runtime = timedelta(seconds=(stop_after + stop_after_variance))
+
+            final_delay = stop_after + random() * stop_after_variance
             log.info("Adding restart watcher after %d seconds", final_delay)
 
             t = StopThread(final_delay, coordinator.request_stop, done_evt)
             t.start()
+
+            for job in to_execute:
+                if job.minimum_interval > max_runtime:
+                    log.error(
+                        "Job runner will be stopped before job can execute",
+                        job_name=job.name,
+                        minimum_interval=job.minimum_interval.total_seconds(),
+                        max_runtime=max_runtime.total_seconds(),
+                    )
+                elif job.maximum_interval > min_runtime:
+                    log.warning(
+                        "Job runner may be stopped before job can execute",
+                        job_name=job.name,
+                        maximum_interval=job.maximum_interval.total_seconds(),
+                        min_runtime=min_runtime.total_seconds(),
+                    )
 
         coordinator.join()
         done_evt.set()
