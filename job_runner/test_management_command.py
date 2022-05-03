becaabd084f4956b479d030c0bc5eb63f3da1f38
Adam Peacock <adam@thepeacock.net>
2022-05-03 14:13:45 -0400

    Add test for timeouts

--- job_runner/test_management_command.py
+++ job_runner/test_management_command.py
@@ -1,17 +1,27 @@
 """Tests for management command"""
 
-from django.core.management import call_command
+from threading import Event
+
+import pytest
+
+from django.core.management import call_command, CommandError
 
 from job_runner.environment import RunEnv
 from job_runner.registration import register_job
 
 test_val = None
+run_forever_event = Event()
 
 
 @register_job(1)
 def set_test_val(env: RunEnv):
     global test_val
     test_val = True
+
+
+@register_job(1)
+def run_forever(env: RunEnv):
+    run_forever_event.wait()
 
 
 def test_management_command_smoke():
@@ -30,3 +40,19 @@ def test_management_command_simple_execution():
     )
 
     assert test_val
+
+
+def test_run_forever_bail():
+    run_forever_event.clear()  # reset
+
+    with pytest.raises(CommandError):
+        call_command(
+            "run_jobs",
+            "--stop-after",
+            "1",
+            "--include-job",
+            "job_runner.test_management_command.run_forever",
+        )
+
+    # clean up the thread by setting the event
+    run_forever_event.set()
