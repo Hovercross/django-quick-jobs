79feb26cb912a3d5dcfe56539eb48353d3800e36
Adam Peacock <adam@thepeacock.net>
2022-05-06 21:43:12 -0400

    Unused jobs were lowering my test coverage

--- job_runner/sample_jobs.py
+++ job_runner/sample_jobs.py
@@ -8,18 +8,3 @@ from job_runner.environment import RunEnv
 @register_job(5, 0)
 def sample_job_1(env: RunEnv):
     print(f"sample_job_1 is getting called at {datetime.now()}")
-
-
-@register_job(1)
-def fatal(env: RunEnv):
-    """A sample job that requests a fatal exit"""
-
-    env.request_fatal_errors()
-    raise Exception("I'm in danger!")
-
-
-@register_job(5)
-def exception(env: RunEnv):
-    """A sample job that throws an exception"""
-
-    raise Exception("I'm not in that much danger")
--- job_runner/test_tracker.py
+++ job_runner/test_tracker.py
@@ -2,13 +2,11 @@
 
 from datetime import timedelta
 
-from .sample_jobs import sample_job_1, exception, fatal
-from .registration import import_default_jobs, import_jobs_from_modules
+from .sample_jobs import sample_job_1
+from .registration import import_jobs_from_modules
 
 
 def test_explicit_jobs():
     jobs = import_jobs_from_modules(["job_runner.sample_jobs"])
     assert sample_job_1 in jobs
-    assert exception in jobs
-    assert fatal in jobs
     assert sample_job_1.interval == timedelta(seconds=5)
