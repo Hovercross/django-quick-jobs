534f38664a3bce3fb0918ff347cb158ff40a0171
Adam Peacock <adam@thepeacock.net>
2022-05-06 21:10:17 -0400

    Add a bunch of management command tests

--- job_runner/test_management_command.py
+++ job_runner/test_management_command.py
@@ -4,13 +4,77 @@ from threading import Event
 
 import pytest
 
-from django.core.management import call_command, CommandError
+from django.core.management import call_command
 
-from job_runner.environment import RunEnv
+from job_runner.environment import RunEnv, SleepInterrupted
 from job_runner.registration import register_job
 
 test_val = None
 run_forever_event = Event()
+slow_job_count = 0
+fast_job_count = 0
+rerun_job_count = 0
+
+
+@register_job(0)
+def paused_job(env: RunEnv):
+    """A job that pauses forever"""
+
+    while True:
+        env.sleep(1)
+
+
+@register_job(0)
+def paused_job_exception(env: RunEnv):
+    """A job used to test if a paused job throws a sleep interrupted"""
+
+    with pytest.raises(SleepInterrupted):
+        while True:
+            env.sleep(1)
+
+
+@register_job(0)
+def stopping_loop_job(env: RunEnv):
+    while not env.is_stopping:
+        pass
+
+
+@register_job(0)
+def fatal_error_job(env: RunEnv):
+    env.request_fatal_errors()
+    raise Exception("I like pie")
+
+
+@register_job(0)
+def stop_request_job(env: RunEnv):
+    env.request_stop()
+
+
+@register_job(0)
+def sleep_job(env: RunEnv):
+    env.sleep(300)
+
+
+@register_job(30)
+def rerun_job(env: RunEnv):
+    global rerun_job_count
+
+    rerun_job_count += 1
+    env.request_rerun()
+
+
+@register_job(30)
+def slow_job(env: RunEnv):
+    global slow_job_count
+
+    slow_job_count += 1
+
+
+@register_job(0.1)
+def fast_job(env: RunEnv):
+    global fast_job_count
+
+    fast_job_count += 1
 
 
 @register_job(1)
@@ -96,3 +160,113 @@ def test_fatal_job():
             "--include-job",
             "job_runner.test_management_command.fatal",
         )
+
+
+def test_fast_job():
+    global fast_job_count
+    fast_job_count = 0
+
+    call_command(
+        "run_jobs",
+        "--stop-after",
+        "1",
+        "--include-job",
+        "job_runner.test_management_command.fast_job",
+    )
+
+    # We allow a range since timing isn't perfect
+    assert 8 < fast_job_count < 12
+
+
+def test_slow_job():
+    global slow_job_count
+    slow_job_count = 0
+
+    call_command(
+        "run_jobs",
+        "--stop-after",
+        "1",
+        "--include-job",
+        "job_runner.test_management_command.slow_job",
+    )
+
+    assert slow_job_count == 1
+
+
+def test_rerun_job():
+    global rerun_job_count
+    rerun_job_count = 0
+
+    call_command(
+        "run_jobs",
+        "--stop-after",
+        "1",
+        "--include-job",
+        "job_runner.test_management_command.rerun_job",
+    )
+
+    # This is a very inexact measurement, but 10 is an extremely low bound.
+    # On my M1 Mac I got 1154 in one second.
+    assert rerun_job_count > 10
+
+
+@pytest.mark.timeout(10)
+def test_sleep():
+    call_command(
+        "run_jobs",
+        "--stop-after",
+        "1",
+        "--include-job",
+        "job_runner.test_management_command.sleep_job",
+    )
+
+
+@pytest.mark.timeout(10)
+def test_request_stop():
+    call_command(
+        "run_jobs",
+        "--include-job",
+        "job_runner.test_management_command.stop_request_job",
+    )
+
+
+@pytest.mark.timeout(10)
+def test_fatal_exception():
+    with pytest.raises(SystemExit):
+        call_command(
+            "run_jobs",
+            "--include-job",
+            "job_runner.test_management_command.fatal_error_job",
+        )
+
+
+def test_stopping_loop():
+    call_command(
+        "run_jobs",
+        "--stop-after",
+        "1",
+        "--include-job",
+        "job_runner.test_management_command.stopping_loop_job",
+    )
+
+
+def test_paused_job_exception():
+    call_command(
+        "run_jobs",
+        "--stop-after",
+        "1",
+        "--include-job",
+        "job_runner.test_management_command.paused_job_exception",
+    )
+
+
+def test_paused_job():
+    """Make sure that a paused job through up through the job runner properly"""
+
+    call_command(
+        "run_jobs",
+        "--stop-after",
+        "1",
+        "--include-job",
+        "job_runner.test_management_command.paused_job",
+    )
