fe713561649c81cdb30e2720963b76d19b72d143
Adam Peacock <adam@thepeacock.net>
2022-10-21 16:15:53 -0400

    Allow jobs to be disabled
    Occasionally users might want to keep a job around, but disable it based on a flag or a setting. Instead of having to comment out the entire block, they can set the enabled flag on a job to false.

--- job_runner/sample_jobs.py
+++ job_runner/sample_jobs.py
@@ -8,3 +8,8 @@ from job_runner.environment import RunEnv
 @register_job(5, 0)
 def sample_job_1(env: RunEnv):
     print(f"sample_job_1 is getting called at {datetime.now()}")
+
+
+@register_job(5, 0, enabled=False)
+def sample_job_disabled(env: RunEnv):
+    print(f"sample_job_disabled is getting called at {datetime.now()}")
